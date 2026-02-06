f2c6c9dba43702c5fdea995dc16f8b2d63187d12
jinseoplee <jjjp9966@gmail.com>
2026-02-06 16:27:28 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/io/github/jinseoplee/coupon/FirstComeFirstServedCouponSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.jinseoplee.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeFirstServedCouponSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstComeFirstServedCouponSystemApplication.class, args);
+	}
+
+}
