ed2a68980a98c825e39997266d501da1b30a7fac
jinseoplee <jjjp9966@gmail.com>
2026-02-06 21:54:36 +0900

    refactor: 쿠폰 발급 중복 조회를 exists 쿼리로 변경

--- src/main/java/io/github/jinseoplee/coupon/issue/CouponIssueRepository.java
+++ src/main/java/io/github/jinseoplee/coupon/issue/CouponIssueRepository.java
@@ -2,9 +2,7 @@ package io.github.jinseoplee.coupon.issue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CouponIssueRepository extends JpaRepository<CouponIssue, Long> {
 
-    Optional<CouponIssue> findByCouponCampaign_IdAndUserId(Long campaignId, Long userId);
+    boolean existsByCouponCampaign_IdAndUserId(Long campaignId, Long userId);
 }
